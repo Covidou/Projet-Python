86960b20060d3baaf498afe7843e313f4ca9b534
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2020-05-04 11:28:38 +0200

    Completed the models in models.py

--- models.py
+++ models.py
@@ -5,22 +5,30 @@
 from django.db import models
 
 class Product(models.Model):
-	id = models.CharField(max_length=8, primary_key=True)
+	productId = models.CharField(max_length=8, primary_key=True)
 	nom = models.CharField(max_length=20)
 
 class User(models.Model):
-	id = models.CharField(max_length=8, primary_key=True)
+	userId = models.CharField(max_length=8, primary_key=True)
 	email = models.CharField(max_length=30, default='name@email.com')
 	phone = models.CharField(max_length=10, default='0000000000')
 	numberOfPeople = models.IntegerField(default=1)
 	address = models.CharField(max_length=50)
 	password = models.CharField(max_length=50, default='password')
+	cityId = models.ForeignKey(City, on_delete=models.DO_NOTHING)
 
 class City(models.Model):
-	id = models.CharField(max_length=8, primary_key=True)
+	cityId = models.CharField(max_length=8, primary_key=True)
 	postalCode = models.CharField(max_length=5, default='00000')
 	name = models.CharField(max_length=20)
 
 class District(models.Model):
-	id = models.CharField(max_length=8, primary_key=True)
+	districtId = models.CharField(max_length=8, primary_key=True)
 	name = models.CharField(max_length=20)
+	cityId = models.ForeignKey(City, on_delete=models.DO_NOTHING)
+
+class Order(models.Model):
+	orderId = models.CharField(max_length=8, primary_key=True)
+	userId = models.ManyToManyField(User)
+	cityId = models.ManyToManyField(City)
+	orderDate = models.DateTimeField(auto_now=False, auto_now_add=True)
